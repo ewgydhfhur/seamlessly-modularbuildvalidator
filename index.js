b2c77ff48966e779f286ddbf2c86b95090c16b5d
ewgydhfhur <msjcikeaeung555@hotmail.com>
2024-05-07 07:07:30 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-function combine(n, k) {
-  const result = [];
-  backtrack(1, []);
-  return result;
-  function backtrack(start, current) {
-    if (current.length === k) {
-      result.push([...current]);
-      return;
-    }
-    for (let i = start; i <= n; i++) {
-      current.push(i);
-      backtrack(i + 1, current);
-      current.pop();
-    }
+function reverseList(head) {
+  let prev = null;
+  let curr = head;
+  while (curr) {
+    const next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
   }
+  return prev;
 }
